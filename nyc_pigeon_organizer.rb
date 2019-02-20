7d1b041e3641c10d6398291a4ac5800fa05406be
Matthew Shin <mshin1995@gmail.com>
2019-02-20 05:55:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |info, options|
+    options.each do |option, names|
+      names.each do |names|
+        new_hash[names] ||= {}
+        new_hash[names][info] ||= []
+        new_hash[names][info] << option.to_s 
+      end
+    end
+  end
+  new_hash
 end
