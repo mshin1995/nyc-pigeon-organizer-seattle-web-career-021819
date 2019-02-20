92de081a3f9abe39a1aa04ba372b6066d691ebbc
Matthew Shin <mshin1995@gmail.com>
2019-02-20 05:47:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,8 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |info, color|
     color.each do |color, name|
-      new_hash[name] 
+      new_hash[name] ||= color
+      new_hash[:gender] ||= []
+      new_hash[:gender] << 
     
 end
